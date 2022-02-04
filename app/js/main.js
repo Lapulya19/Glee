7e3be72b3a1df014caec0019eb829fdf207fddbd
lapulya 69 <sveta.serdyuk2003@gmail.com>
2022-02-04 20:59:48 +0200

    added filter

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,26 @@
 $(function(){
- 
+  $('.filter-recent__star').rateYo({
+    starWidth: '11px',
+    normalFill:'#d6d6d6',
+    ratedFill:'#ffcc00',
+    readOnly:true,
+    spacing   : "8px",
+  });
+ $('.filter-price__input').ionRangeSlider({
+   type: 'double',
+   onStart: function (data){
+    $('.filter-price__from').text(data.from);
+    $('.filter-price__to').text(data.to);
+   },
+   onChange: function (data) {
+    $('.filter-price__from').text(data.from);
+    $('.filter-price__to').text(data.to);
+  },
+
+ })
+
+
+
   $('.slider-top__items').slick({
    arrows: false,
    dots:true,
@@ -9,12 +30,7 @@ $(function(){
   var mixer = mixitup('.products__all');
   var mixer = mixitup('.design__all');
 
-  // $("").rateYo({
-  //   rating:3.6,only js
-  // starWidtn:"40px",
-  // normalFill:"#ccc",
-  // ratedFill:"#fff",
-  // readOnly:true, disabled changing
-  // });
-  // data-rateyo-rating="4" only html
+  
+  // data-rateyo-rating="4"
+  //  only html
 })
