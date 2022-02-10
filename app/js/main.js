82a9eb9b9bae24e78e840eebac142d1ced645e71
lapulya 69 <sveta.serdyuk2003@gmail.com>
2022-02-10 17:28:47 +0200

    added page product-detailes

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
 $(function () {
+
   $('.filter-recent__star').rateYo({
     starWidth: '11px',
     normalFill: '#d6d6d6',
@@ -6,7 +7,7 @@ $(function () {
     readOnly: true,
     spacing: "8px",
   });
-  $('.product-list__rating').rateYo({
+  $('.product-list__rating, .product-details__rating').rateYo({
     starWidth: '18px',
     normalFill: '#d6d6d6',
     ratedFill: '#ffcc00',
@@ -24,8 +25,24 @@ $(function () {
       $('.filter-price__to').text(data.to);
     },
 
-  })
+  });
 
+
+  $('.slider-thumb').slick({
+    asNavFor: '.slider-big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false
+  });
+  $('.slider-big').slick({
+    asNavFor: '.slider-thumb',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+  });
 
 
   $('.slider-top__items').slick({
@@ -33,6 +50,15 @@ $(function () {
     dots: true,
     fade: true,
   });
+
+   //-----------------Slider related-------------//
+   $('.related__products').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    variableWidth: true,
+    nextArrow:'<button type="button" class="slick-next"><img src="images/slider/arrows-right.svg" ></button>',
+    prevArrow:'<button type="button" class="slick-prev"><img src="images/slider/arrows-left.svg" ></button>',
+   });
 
   $('.product-list__btn').on('click', function () {
     $('.product-list__btn').removeClass('product-list__btn--active')
@@ -46,6 +72,23 @@ $(function () {
   $('.button-grid').on('click', function () {
     $('.product-list__all-item').removeClass('product-list__all-item--line')
   });
+
+  //-----------------Form Styler-------------//
+
+  $('.product-details__input').styler();
+
+  //-----------------Tabs-------------//
+
+  $('.product-details__item').on('click', function (e) {
+    e.preventDefault();
+    $('.product-details__item').removeClass('product-details__item--active');
+    $(this).addClass('product-details__item--active');
+
+    $('.product-details__content-item').removeClass('product-details__content-item--active');
+    $($(this).attr('href')).addClass('product-details__content-item--active');
+  });
+
+  //-----------------Mixitup-------------//
 
   var mixer = mixitup('.products__all');
   var mixer = mixitup('.design__all');
